668aa47c564c0ee84857574c9018f9ff1f3f4487
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2024-04-24 15:18:34 +0530

    Add self signed certificate

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(helmet());
 
-app.get("/secret", checkLoggedIn, (req, res) => {
+app.get("/secret", (req, res) => {
   return res.send("Your personal secret value is 42!");
 });
 
@@ -18,6 +18,11 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-https.createServer({ key: "", cert: "" }, app).listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
+https
+  .createServer(
+    { key: fs.readFileSync("key.pem"), cert: fs.readFileSync("cert.pem") },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
